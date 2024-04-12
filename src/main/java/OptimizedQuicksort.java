727fa4062db4cbf1fd0d664335f0f8647316795b
adrianarmm <areyemor@myuax.com>
2024-04-12 16:39:25 +0200

    30

---
+++ src/main/java/OptimizedQuicksort.java
@@ -0,0 +1,51 @@
+public class OptimizedQuicksort {
+    public static void quicksort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(arr, low, high);
+            quicksort(arr, low, pivotIndex - 1);
+            quicksort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = medianOfThree(arr, low, high);
+        int i = low - 1;
+        int j = high + 1;
+
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < pivot);
+
+            do {
+                j--;
+            } while (arr[j] > pivot);
+
+            if (i >= j) {
+                return j;
+            }
+
+            swap(arr, i, j);
+        }
+    }
+
+    private static int medianOfThree(int[] arr, int low, int high) {
+        int mid = (low + high) / 2;
+        if (arr[low] > arr[mid]) {
+            swap(arr, low, mid);
+        }
+        if (arr[low] > arr[high]) {
+            swap(arr, low, high);
+        }
+        if (arr[mid] > arr[high]) {
+            swap(arr, mid, high);
+        }
+        return arr[mid];
+    }
+
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
