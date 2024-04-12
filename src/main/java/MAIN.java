af2bf2aac18587fb38d95a64849c1d29e14cad05
adrianarmm <areyemor@myuax.com>
2024-04-12 17:26:45 +0200

    42

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -130,6 +130,22 @@ public class MAIN {
         return -1;
 
 }
-
-
+    public static class MaximoPosicion {
+        public static int maximo(int[] datos, int indice, int[] posiciones) {
+            if (indice == 0) {
+                posiciones[0] = 0;
+                return datos[0];
+            } else {
+                int maximoAnterior = maximo(datos, indice - 1, posiciones);
+                if (datos[indice] > maximoAnterior) {
+                    posiciones[0] = indice;
+                    return datos[indice];
+                } else {
+                    return maximoAnterior;
+                }
+            }
+        }
+    }
 }
+
+
