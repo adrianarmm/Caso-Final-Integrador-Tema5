f92933ed64a934fb10eac4d2f4395448e9e87f91
adrianarmm <areyemor@myuax.com>
2024-04-12 16:41:09 +0200

    31

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,3 +1,10 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MAIN {
 
     public static void main(String[] args) {
@@ -7,20 +14,42 @@ public class MAIN {
         int numeroGenes = contarGenes(cadenaADN, posicion);
         System.out.println("El número de genes en la cadena de ADN es: " + numeroGenes);
 
-   System.out.println("Combinaciones Genéticas");
+        System.out.println("Combinaciones Genéticas");
         int numeroGenes2 = 2;
         int numeroAlelos = 4;
         int combinacionesGeneticas = CombinacionesGeneticas(numeroGenes, numeroAlelos);
         System.out.println("El número de combinaciones genéticas posibles es: " + combinacionesGeneticas);
+
+        System.out.println("Búsqueda Binaria");
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/adrianareyesmorera/Desktop/CASO5.txt"))) {
+            String linea;
+            List<String> listaLineas = new ArrayList<>();
+            while ((linea = br.readLine()) != null) {
+                listaLineas.add(linea);
+            }
+            // Convierte la lista a un arreglo para poder usar búsqueda binaria
+            String[] lineas = listaLineas.toArray(new String[0]);
+            Arrays.sort(lineas); // Ahora es seguro ordenar el arreglo, ya que no contiene nulls
+            String palabraBuscada = "ADN";
+            int posicion2 = Arrays.binarySearch(lineas, palabraBuscada);
+            if (posicion2 >= 0) {
+                System.out.println("La palabra '" + palabraBuscada + "' se encuentra en la posición " + posicion2);
+            } else {
+                System.out.println("La palabra '" + palabraBuscada + "' no se encuentra en el archivo");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
     public static int contarGenes(String cadenaADN, int posicion) {
         if (cadenaADN == null || cadenaADN.isEmpty()) {
             return 0;
         }
-    if (!cadenaADN.contains("ATG")) {
+        if (!cadenaADN.contains("ATG")) {
             return 0;
         }
-    int finGen = Math.max(cadenaADN.indexOf("TGA", posicion), cadenaADN.indexOf("TAG", posicion));
+        int finGen = Math.max(cadenaADN.indexOf("TGA", posicion), cadenaADN.indexOf("TAG", posicion));
 
         if (finGen == -1) {
             return 0;
