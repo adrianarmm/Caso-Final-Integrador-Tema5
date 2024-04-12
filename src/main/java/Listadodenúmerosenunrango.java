f8551403ee3279248ad3bd7d11976c44fa3b0095
adrianarmm <areyemor@myuax.com>
2024-04-12 16:08:51 +0200

    11

--- src/main/java/Listadodenúmerosenunrango.java
+++ src/main/java/Listadodenúmerosenunrango.java
@@ -6,5 +6,12 @@ public class Listadodenúmerosenunrango {
                 listar(inicio + 1, fin);
             }
         }
+
+        public static void main(String[] args) {
+            int inicio = 1;
+            int fin = 10;
+            System.out.print("Los números en el rango de " + inicio + " a " + fin + " son: ");
+            listar(inicio, fin);
+        }
     }
 
