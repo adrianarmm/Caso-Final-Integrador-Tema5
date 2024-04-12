86969f9908f5df8d7b1745b19887b8a726103506
adrianarmm <areyemor@myuax.com>
2024-04-12 16:13:37 +0200

    13

---
+++ src/main/java/Potencias.java
@@ -0,0 +1,9 @@
+public class Potencias {
+    public static long potencia(int base, int exponente) {
+        if (exponente == 0) {
+            return 1;
+        } else {
+            return base * potencia(base, exponente - 1);
+        }
+    }
+}
