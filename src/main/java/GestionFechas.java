a8c645e1d2de20263506a3aafb912b907aee8355
adrianarmm <areyemor@myuax.com>
2024-04-12 16:37:40 +0200

    28

---
+++ src/main/java/GestionFechas.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class GestionFechas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<LocalDate> fechas = new ArrayList<>();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        while (true) {
+            System.out.print("Introduce una fecha (20/08/2005) o escribe 'fin' para terminar: ");
+            String entrada = scanner.nextLine();
+            if (entrada.equalsIgnoreCase("fin")) {
+                break;
+            }
+            LocalDate fecha = LocalDate.parse(entrada, formatter);
+            fechas.add(fecha);
+        }
+        Collections.sort(fechas);
+        System.out.println("Fechas ordenadas:");
+        for (LocalDate fecha : fechas) {
+            System.out.println(fecha.format(formatter));
+        }
+    }
+}
