08c80b5e01aff0592aafe91a2f394595c86eca4c
adrianarmm <areyemor@myuax.com>
2024-04-12 13:58:13 +0200

    4

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -1,2 +1,13 @@
 public class MAIN {
+
+    public static void main(String[] args) {
+        System.out.println("Conteo de Genes");
+        String cadenaADN = "ATGCGTAGATGCGATAG";
+        int posicion = 0;
+        int numeroGenes = contarGenes(cadenaADN, posicion);
+        System.out.println("El número de genes en la cadena de ADN es: " + numeroGenes);
+
+    }
+
+
 }
