cba51571d206d9a437b6874481ccae3910a4e046
adrianarmm <areyemor@myuax.com>
2024-04-12 16:35:03 +0200

    26

--- src/main/java/BusquedaBinaria.java
+++ src/main/java/BusquedaBinaria.java
@@ -1,25 +1,27 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class BusquedaBinaria {
     public static void main(String[] args) {
-        try (BufferedReader br = new BufferedReader(new FileReader("archivo.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("/Users/adrianareyesmorera/Desktop/CASO5.txt"))) {
             String linea;
-            int contador = 0;
-            String[] lineas = new String[100];
+            List<String> listaLineas = new ArrayList<>();
             while ((linea = br.readLine()) != null) {
-                lineas[contador] = linea;
-                contador++;
+                listaLineas.add(linea);
             }
-            Arrays.sort(lineas);
+            // Convierte la lista a un arreglo para poder usar búsqueda binaria
+            String[] lineas = listaLineas.toArray(new String[0]);
+            Arrays.sort(lineas); // Ahora es seguro ordenar el arreglo, ya que no contiene nulls
             String palabraBuscada = "ejemplo";
             int posicion = Arrays.binarySearch(lineas, palabraBuscada);
             if (posicion >= 0) {
-                System.out.println("La palabra " + palabraBuscada + " se encuentra en la posición " + posicion);
+                System.out.println("La palabra '" + palabraBuscada + "' se encuentra en la posición " + posicion);
             } else {
-                System.out.println("La palabra " + palabraBuscada + " no se encuentra en el archivo");
+                System.out.println("La palabra '" + palabraBuscada + "' no se encuentra en el archivo");
             }
         } catch (IOException e) {
             e.printStackTrace();
