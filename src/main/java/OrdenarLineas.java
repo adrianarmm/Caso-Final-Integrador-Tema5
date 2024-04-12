b5306cd100889a2da06d53efe4c0206a855fd79b
adrianarmm <areyemor@myuax.com>
2024-04-12 16:24:27 +0200

    21

--- src/main/java/OrdenarLineas.java
+++ src/main/java/OrdenarLineas.java
@@ -23,12 +23,4 @@ public class OrdenarLineas {
         }
     }
 
-    public static void main(String[] args) {
-        int[] datos = { 1, 2, 3, 4, 5 };
-        int[] posiciones = { 0 };
-        int indice = datos.length - 1;
-        int resultado = MaximoPosicion.maximo(datos, indice, posiciones);
-        System.out.println("El número máximo en el arreglo es: " + resultado);
-        System.out.println("La posición del número máximo en el arreglo es: " + posiciones[0]);
-    }
 }
