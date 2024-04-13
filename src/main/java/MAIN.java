879ef9934a85b3b0eaab1346126d781d432c793b
adrianarmm <areyemor@myuax.com>
2024-04-13 18:51:23 +0200

    70

--- src/main/java/MAIN.java
+++ src/main/java/MAIN.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.Scanner;
 import java.util.*;
 
 public class MAIN {
